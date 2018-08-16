f4829be94bd0af1c192cbcb59336e54cb26f837e
BRobat <Patogeen@icloud.com>
2018-08-16 21:09:50 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { BoardComponent } from './board/board.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    FormComponent,
+    BoardComponent,
+    SuccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: "", component: FormComponent},
+      {path: "success", component: SuccessComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
